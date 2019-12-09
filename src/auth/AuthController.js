e3eb505b722654bee943d0b422da8748bf53cc6e
Cristian <holme027@rangers.uwp.edu>
2019-12-09 14:54:07 -0600

    Fix Tokens

--- src/auth/AuthController.js
+++ src/auth/AuthController.js
@@ -11,23 +11,26 @@ var bcrypt = require('bcryptjs');
 var config = require('../../config');
 
 router.post('/register', function (req, res) {
+    const hashedPassword = bcrypt.hashSync(req.body.password, 8)
 
-    var encryptedPassword = bcrypt.hashSync(req.body.password, 8);
     User.create({
-            name : req.body.name,
-            email : req.body.email,
-            password : hashedPassword,
-            admin : req.body.admin
+            firstName: req.body.firstName,
+            lastName: req.body.lastName,
+            email: req.body.email,
+            password: hashedPassword,
+            admin: req.body.admin,
+            score: 0,
+            locations: []
         },
         function (err, user) {
-            if (err) return res.status(500).send("There was a problem registering the user.")
+            if (err) return res.status(500).send('There was a problem registering the user.')
             // create a token
-            var token = jwt.sign({ id: user._id }, config.secret, {
-                expiresIn: 86400 // expires in 24 hours
-            });
-            res.status(200).send({ auth: true, token: token });
-        });
-});
+            var token = jwt.sign({ id: user._id }, process.env.JWT_SECRET, {
+                expiresIn: 172800 // expires in 48 hours
+            })
+            res.status(200).send({ auth: true, token: token })
+        })
+})
 
 router.post('/login', function(req, res) {
     User.findOne({ email: req.body.email }, function (err, user) {
